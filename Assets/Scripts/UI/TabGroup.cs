d4d6ecaf63a1d23f9422378c635a0eb026c7cce1
Peter Lakatos <lakatopeter8@gmail.com>
2023-10-16 21:52:57 +0200

    Started working on framework UI. Basic logic for tab selection. Added fonts.

---
+++ Assets/Scripts/UI/TabGroup.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabGroup : MonoBehaviour
+{
+  public List<TabButton> tabButtons;
+  private TabButton _selectedTab;
+  public List<GameObject> objectsToSwap;
+
+  public void Subscribe(TabButton button)
+  {
+    if (tabButtons == null)
+    {
+      tabButtons = new List<TabButton>();
+    }
+
+    tabButtons.Add(button);
+  }
+
+  public void OnTabEnter(TabButton button)
+  {
+
+  }
+
+  public void OnTabExit(TabButton button)
+  {
+
+  }
+
+  public void OnTabSelected(TabButton button)
+  {
+    _selectedTab = button;
+    int index = button.transform.GetSiblingIndex();
+    for (int i =0; i < objectsToSwap.Count; i++)
+    {
+      if (i == index)
+      {
+        objectsToSwap[i].SetActive(true);
+      }
+      else
+      {
+        objectsToSwap[i].SetActive(false);
+      }
+    }
+  }
+}
