2a9221774f02fa457f03f03d82fec7fc0c214ea1
Peter Lakatos <lakatopeter8@gmail.com>
2023-11-20 08:56:17 +0100

    Created new job struct for GA

---
+++ Assets/Scripts/Agent/BasicEAAgent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Basic EA agent
+/// For collision avoidance and path planning use NavMeshAgent (RVO + A*)
+/// </summary>
+public class BasicEAAgent : BaseAgent
+{
+  public override IBaseCollisionAvoider collisionAlg { get; set; }
+  public override IBasePathPlanner pathPlanningAlg { get; set; }
+
+  public BasicEAAgent()
+  {
+    collisionAlg = SimulationManager.Instance._collisionManager.GetOrCreateCollisionAlg<ORCACollision>(() => new ORCACollision(this));
+    pathPlanningAlg = new NavMeshPathPlanner(this);
+  }
+
+  public override void SetDestination(Vector2 des)
+  {
+  }
+
+  // Stop EA evaluation and set desired direction so that collision avoidance alg can process it
+  public override void OnBeforeUpdate()
+  {
+  }
+
+  // Set agent position and start new EA cycle
+  public override void OnAfterUpdate(Vector2 newPos)
+  {
+  }
+}
---
+++ Assets/Scripts/EAJobs/GeneticAlgorithmJob.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+public struct GeneticAlgorithmJob : IJob
+{ 
+  public bool stopExecution;
+  [ReadOnly] public int index;
+  [ReadOnly] public float timeDelta;
+  [ReadOnly] public NativeArray<Vector2> positions;
+  [ReadOnly] public NativeArray<float> speeds;
+  [ReadOnly] public NativeArray<UnsafeList<Vector2>> paths;
+
+  public void Execute()
+  {
+    if (stopExecution)
+      return;
+
+    InitializePopulation();
+    while (!stopExecution)
+    {
+      CalculateFitness();
+      ApplySelection();
+      ApplyOperators();
+      SelectNewPopulation();
+    }
+  }
+
+  private void InitializePopulation()
+  {
+
+  }
+
+  private void CalculateFitness()
+  {
+
+  }
+
+  private void ApplySelection()
+  {
+
+  }
+
+  private void ApplyOperators()
+  {
+
+  }
+
+  private void SelectNewPopulation()
+  {
+
+  }
+}
