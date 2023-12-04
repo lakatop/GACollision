836afb6264c0c5efebd8c372c46c594449a3da5f
Peter Lakatos <lakatopeter8@gmail.com>
2023-12-04 12:09:13 +0100

    Basic selection

--- Assets/Scripts/GeneticAlgorithm/BasicGeneticAlgorithm.cs
+++ Assets/Scripts/GeneticAlgorithm/BasicGeneticAlgorithm.cs
@@ -5,6 +5,7 @@ using UnityEngine.Assertions;
 using Unity.Collections;
 using NativeQuadTree;
 using UtilsGA;
+using System.Linq;
 
 public class BasicIndividual
 {
@@ -330,13 +331,59 @@ public class BasicFitnessFunction : IPopulationModifier<BasicIndividual>
 
 public class BasicSelectionFunction : IPopulationModifier<BasicIndividual>
 {
+  System.Random _rand = new System.Random();
+
   public IPopulation<BasicIndividual> ModifyPopulation(IPopulation<BasicIndividual> currentPopulation)
   {
-    return new BasicPopulation();
+    List<BasicIndividual> selectedPop = new List<BasicIndividual>();
+    var population = currentPopulation.GetPopulation();
+
+    int multiplier = 10000;
+
+    // Apply roulette selection
+    double totalFitness = population.Sum(x => System.Math.Round(x.fitness * multiplier));
+
+    if (totalFitness == 0)
+    {
+      return currentPopulation;
+    }
+
+    List<double> relativeFitnesses = population.Select(x => Mathf.Round(x.fitness * multiplier) / totalFitness).ToList();
+
+    List<double> wheel = new List<double>();
+    double prob = 0f;
+    foreach (var fit in relativeFitnesses)
+    {
+      prob += fit;
+      wheel.Add(prob);
+    }
+
+    for (int i = 0; i < population.Length; i++)
+    {
+      double val = _rand.NextDouble();
+      int index = 0;
+      foreach (var wheelVal in wheel)
+      {
+        if (val < wheelVal)
+        {
+          break;
+        }
+        index++;
+      }
+
+      // In case we are dealing with really small fitnesses ->
+      // their sum might not give 1.0 and then theres chance that index will be equal to population size ->
+      // clamp in to last one
+      index = Mathf.Clamp(index, 0, population.Length - 1);
+
+      selectedPop.Add(population[index]);
+    }
+
+    currentPopulation.SetPopulation(selectedPop.ToArray());
+    return currentPopulation;
   }
 
   public void SetResources(List<object> resources)
   {
-    throw new System.NotImplementedException();
   }
 }
